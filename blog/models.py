1c64e42e62cc0c9808d05f68aa44074eb74c3a2d
Sadik Mohamud <msadiknur@gmail.com>
2026-02-23 14:45:18 +0000

    Fix indentation in Meta class and update __str__ method in post model

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -14,11 +14,31 @@ class post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-class Meta:
-     ordering = ['-created_on']
 
-     def __str__(self):
-         return f"{self.title} | written by {self.author}"
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
     
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_author"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
