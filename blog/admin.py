96eb68a80687e064f56a997f26831e8cef3744f1
Sadik Mohamud <msadiknur@gmail.com>
2026-02-23 15:02:47 +0000

    Fix model import case sensitivity in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import post, Comment
+from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(post)
+admin.site.register(Post)
 admin.site.register(Comment)
