bb01f3a418ae169338df17542593f8d2c83e7022
Sadik Mohamud <msadiknur@gmail.com>
2026-02-23 14:37:19 +0000

    Fix indentation in Meta class and update __str__ method in post model

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,11 @@ class post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    class Meta:
-        ordering = ['-created_on']
+class Meta:
+     ordering = ['-created_on']
+
+     def __str__(self):
+         return f"{self.title} | written by {self.author}"
+    
 
 
