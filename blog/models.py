7c535add68d047fd6c9b57dba14f0d28c7abe8d4
Sadik Mohamud <msadiknur@gmail.com>
2026-02-24 15:40:23 +0000

    Refactor Post model structure and improve string representation

--- blog/models.py
+++ blog/models.py
@@ -9,16 +9,18 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User,on_delete=models.CASCADE, related_name='blog_posts' )
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    field_2 = models.CharField(default= 'Hello, World!')
+    field_3 = models.CharField(null = True)
+
     class Meta:
         ordering = ['-created_on']
-        def __str__(self):
-         return f"{self.title} | written by {self.author}"    
-        content = models.TextField()
-        excerpt = models.TextField(blank=True)
-        created_on = models.DateTimeField(auto_now_add=True)
-        status = models.IntegerField(choices=STATUS, default=0)
-        field_2 = models.CharField(default= 'Hello, World!')
-        field_3 = models.CharField(null = True)  
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
     
 
