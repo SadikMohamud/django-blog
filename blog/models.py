ba90f499750375b4d494bb5a2dd371a29b455d04
Sadik Mohamud <msadiknur@gmail.com>
2026-02-24 15:05:58 +0000

    Refactor Post model structure and adjust Comment model foreign key relationships

--- blog/models.py
+++ blog/models.py
@@ -9,33 +9,24 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User,on_delete=models.CASCADE, related_name='blog_posts' )
-    
-    content = models.TextField()
-    excerpt = models.TextField(blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    field_2 = models.CharField(default= 'Hello, World!')
-    field_3 = models.CharField(null = True)  
-
-
     class Meta:
         ordering = ['-created_on']
+        def __str__(self):
+         return f"{self.title} | written by {self.author}"    
+        content = models.TextField()
+        excerpt = models.TextField(blank=True)
+        created_on = models.DateTimeField(auto_now_add=True)
+        status = models.IntegerField(choices=STATUS, default=0)
+        field_2 = models.CharField(default= 'Hello, World!')
+        field_3 = models.CharField(null = True)  
 
-    def __str__(self):
-        return f"{self.title} | written by {self.author}"
     
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="comments_author"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
